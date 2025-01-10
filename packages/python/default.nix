fb6e4d78e0a98134a0bad389f30b316765df6e57
Dessera <dessera@qq.com>
2025-01-11 00:40:13 +0800

    真正修复了ruff问题

--- packages/python/default.nix
+++ packages/python/default.nix
@@ -2,37 +2,23 @@
   nixcodeLib,
   codeExtensions,
   originalPackages,
-
-  pkgs,
   ...
 }:
 let
-  nixLib = pkgs.lib;
-
   inherit (nixcodeLib) mkCode;
 in
-nixLib.makeOverridable (
-  { python, ... }@overrideParams:
-  (mkCode {
-    settings = builtins.fromJSON (builtins.readFile ./settings.json);
-    extensions = (
-      with codeExtensions.open-vsx;
-      [
-        ms-python.python
-        ms-python.debugpy
-        ms-pyright.pyright
-        charliermarsh.ruff
-        njpwerner.autodocstring
-        kevinrose.vsc-python-indent
-      ]
-    );
-    startupScripts = [
-      ''
-        mkdir -p $PWD/.vscode
-        echo '{ "ruff.interpreter": [ "${python}/bin/python" ] }' > $PWD/.vscode/settings.json
-      ''
-    ];
-    deriveFrom = [ originalPackages.nix ];
-  }).override
-    (builtins.removeAttrs overrideParams [ "python" ])
-) { python = pkgs.python3; }
+mkCode {
+  settings = builtins.fromJSON (builtins.readFile ./settings.json);
+  extensions = (
+    with codeExtensions.open-vsx;
+    [
+      ms-python.python
+      ms-python.debugpy
+      ms-pyright.pyright
+      charliermarsh.ruff
+      njpwerner.autodocstring
+      kevinrose.vsc-python-indent
+    ]
+  );
+  deriveFrom = [ originalPackages.nix ];
+}
