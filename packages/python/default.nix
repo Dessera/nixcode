08afbdb04392409fb51bdf7fec001a7d0b64b459
Dessera <dessera@qq.com>
2025-01-19 20:15:22 +0800

    更新python配置

--- packages/python/default.nix
+++ packages/python/default.nix
@@ -2,23 +2,26 @@
   nixcodeLib,
   codeExtensions,
   originalPackages,
+
+  pkgs,
   ...
 }:
 let
   inherit (nixcodeLib) mkCode;
 in
 mkCode {
+  inherit (pkgs) vscode;
   settings = builtins.fromJSON (builtins.readFile ./settings.json);
-  extensions = (
-    with codeExtensions.open-vsx;
-    [
-      ms-python.python
-      ms-python.debugpy
-      ms-pyright.pyright
+  extensions =
+    (with codeExtensions.open-vsx; [
       charliermarsh.ruff
       njpwerner.autodocstring
       kevinrose.vsc-python-indent
-    ]
-  );
+    ])
+    ++ (with codeExtensions.vscode-marketplace; [
+      ms-python.python
+      ms-python.debugpy
+      ms-python.vscode-pylance
+    ]);
   deriveFrom = [ originalPackages.nix ];
 }
